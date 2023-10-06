728aafbc204d65feb0a5401bf571e1dd53465be8
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-10-05 17:23:31 -0700

    update

--- main.c
+++ main.c
@@ -10,21 +10,29 @@
 
 int main(int argc, char **argv)
 {
-	char *lineptr;
-	size_t n;
-start:
-	lineptr = NULL;
-	n = 0;
+	char *lineptr = NULL;
+	size_t n = 0;
+	ssize_t n_read;
 
 	(void)argc;
 	(void)argv;
 
-	_prompt();
-	getline(&lineptr, &n, stdin);
-	printf("%s", lineptr);
+	while (1)
+	{
+		_prompt();
+		n_read = getline(&lineptr, &n, stdin);
 
-	free(lineptr);
-	goto start;
+		if (n_read == -1)
+		{
+			printf("Exiting shell ...\n");
+
+			return (-1);
+		}
+
+		printf("%s\n", lineptr);
+	
+		free(lineptr);
+	}
 
 	return (0);
 }
