78c9ee02ba6877f1250a52dd083dc725d6cab0cd
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-10-17 12:31:10 -0700

    moving files and funcs from Tests repo to simple_shell

---
+++ bultn_help.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _help - Displaying Help For Builtin
+ * @cmd:Parsed Command
+ * @er: Statue Of Last Command Excuted
+ * Return: 0 Succes -1 Fail
+ */
+
+int _help(char **cmd, __attribute__((unused))int er)
+{
+	int fd, fw, rd = 1;
+	char c;
+
+	fd = open(cmd[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error");
+		return (0);
+	}
+	while (rd > 0)
+	{
+		rd = read(fd, &c, 1);
+		fw = write(STDOUT_FILENO, &c, rd);
+
+		if (fw < 0)
+			return (-1);
+	}
+	_putchar_('\n');
+
+	return (0);
+}
